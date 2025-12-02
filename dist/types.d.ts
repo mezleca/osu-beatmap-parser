d1e06ba75cba717f16bd50042dca9113980b8bc5
mezleca <mtksenpai@gmail.com>
2025-12-02 06:35:59 -0300

    no dist

--- dist/types.d.ts
+++
@@ -1,5 +0,0 @@
-export type OsuKey = "AudioFilename" | "AudioLeadIn" | "PreviewTime" | "Countdown" | "SampleSet" | "StackLeniency" | "Mode" | "LetterboxInBreaks" | "WidescreenStoryboard" | "Bookmarks" | "DistanceSpacing" | "BeatDivisor" | "GridSize" | "TimelineZoom" | "Title" | "TitleUnicode" | "Artist" | "ArtistUnicode" | "Creator" | "Version" | "Source" | "Tags" | "BeatmapID" | "BeatmapSetID" | "HPDrainRate" | "CircleSize" | "OverallDifficulty" | "ApproachRate" | "SliderMultiplier" | "SliderTickRate" | "Background" | "Video" | "Storyboard" | "Duration";
-export interface OsuInput {
-    path: string;
-    id?: string;
-}
