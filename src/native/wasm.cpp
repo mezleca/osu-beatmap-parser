9f83a00e4993688e9cb6d2fee5b0d447d500472e
mezleca <mtksenpai@gmail.com>
2025-12-24 19:37:56 -0300

    another path fix...

--- src/native/wasm.cpp
+++ src/native/wasm.cpp
@@ -8,27 +8,27 @@
 using namespace emscripten;
 
 std::string get_property_wasm(uintptr_t data_ptr, int length, std::string key) {
-  std::string_view content(reinterpret_cast<const char *>(data_ptr), length);
-  return osu_parser::get_property(content, key);
+    std::string_view content(reinterpret_cast<const char *>(data_ptr), length);
+    return osu_parser::get_property(content, key);
 }
 
 val get_properties_wasm(uintptr_t data_ptr, int length, val keys) {
-  std::string_view content(reinterpret_cast<const char *>(data_ptr), length);
+    std::string_view content(reinterpret_cast<const char *>(data_ptr), length);
 
-  std::vector<std::string> key_list = vecFromJSArray<std::string>(keys);
-  auto results = osu_parser::get_properties(content, key_list);
+    std::vector<std::string> key_list = vecFromJSArray<std::string>(keys);
+    auto results = osu_parser::get_properties(content, key_list);
 
-  val obj = val::object();
-  for (const auto &key : key_list) {
-    obj.set(key, results[key]);
-  }
+    val obj = val::object();
+    for (const auto &key : key_list) {
+        obj.set(key, results[key]);
+    }
 
-  return obj;
+    return obj;
 }
 
 EMSCRIPTEN_BINDINGS(osu_parser) {
-  function("get_property", &get_property_wasm);
-  function("get_properties", &get_properties_wasm);
+    function("get_property", &get_property_wasm);
+    function("get_properties", &get_properties_wasm);
 }
 
 int main() { return 0; }
